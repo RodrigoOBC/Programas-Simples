ba42b15776ddf075a581a9078dfffddd305ef9c6
Rodrigo de Brito de Oliveira Cabral <32024711+RodrigoOBC@users.noreply.github.com>
2018-06-24 00:59:33 -0300

    Funçoes matematicas
    Ate o fim do ano serão postadas diversas formulas matemáticas nesse arquivo, para facilitar a vida de todos
    
    Versão 1.0.0 
    
    Classes:
    
    class Triangulo/// class: Quadrado /// calss: Retangulo  /// Circulo /// Trapezio
    
    funções encontradas são:
    Perimetro()
    area()
    diagonal()
    diametro()

---
+++ Funcoes.py
@@ -0,0 +1,59 @@
+####
+#
+# Pensado e realizado pelo Rodrigo de Brito de Oliveira Cabral com a Ajuda da amiga nathy
+#
+####
+
+class Triangulo:
+
+    def perimetro(self, lado):
+        return lado * 3
+
+    def area(self,b,h):
+        return (b*h)/2
+
+    def hipotenusa(self, b, h):
+        return (b**2 + h**2)**0.5
+
+
+class Quadrado:
+
+    def perimetro(self, lado):
+        return lado*4
+
+    def area(self, lado):
+        return lado*2
+
+    def diagonal(self, lado):
+        return self.area(lado)**0.5
+
+
+
+class Retangulo:
+
+    def perimetro(self, lado):
+        return lado * 4
+
+    def area(self, b, h):
+        return b * h
+
+
+class Trapezio:
+    def perimetro(self, lado, b, B):
+        return B + b + (2*lado)
+
+    def area(self, h, b, B):
+        return ((B + b)*h)/2
+
+
+class Circulo:
+    pi = 3,14
+
+    def area(self, r):
+        return self.pi * r**2
+
+    def perimetro(self, r):
+        return (self.pi*2)*r
+
+    def diametro(self, r):
+        return r*2
